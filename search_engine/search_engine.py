a9929349e945725e303db229ace9a0e38c44c4eb
nwhator <nwhator@gmail.com>
2024-07-14 22:47:19 +0100

    chore: Update Flask app to use lazy loading for static files, added SSearch front page with basic css design

--- search_engine/search_engine.py
+++ search_engine/search_engine.py
@@ -1,15 +1,10 @@
 from flask import Flask, render_template
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder="./static/")
 
 @app.route("/")
-@app.route("/home")
-def hello_world():
-    return "<p>Hello, World</p>"
-
-@app.route("/about")
-def about():
-    return "<h1>About</h1>"
+def websearch():
+    return render_template("index.html")
 
 if __name__ == '__main__':
     app.run(debug=True)
