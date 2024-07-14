b1d067bb40e41b1022daa01c1d57ef7e28e9b1dc
nwhator <nwhator@gmail.com>
2024-07-14 23:16:26 +0100

    feat: Add web search functionality to Flask app

---
+++ search_engine/crawler_store_tokens.py
@@ -0,0 +1,4 @@
+import pickle
+import os
+from bs4 import BeautifulSoup
+import requests
--- search_engine/search_engine.py
+++ search_engine/search_engine.py
@@ -1,10 +1,19 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__, template_folder="./static/")
 
 @app.route("/")
 def websearch():
     return render_template("index.html")
+
+@app.route("/websearch", methods = ['GET', 'POST'])
+def web_search():
+    if request.method == 'POST':
+        query = request.form['query']
+        if query == "":
+            return render_template('index.html')
+        
+        return render_template('results.html', data = query)
 
 if __name__ == '__main__':
     app.run(debug=True)
