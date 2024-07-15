8e0dcb83b3ff7da069af789b41cac789fb4ca9aa
nwhator <nwhator@gmail.com>
2024-07-15 03:05:25 +0100

    feat: Add tokenization and saving of tokenized text to Flask app

--- search_engine/search_engine.py
+++ search_engine/search_engine.py
@@ -6,6 +6,26 @@ app = Flask(__name__, template_folder="./static/")
 def websearch():
     return render_template("index.html")
 
+@app.route("/peter")
+def peter():
+    return render_template("a.html")
+
+@app.route("/lois")
+def lois():
+    return render_template("b.html")
+
+@app.route("/chris")
+def chris():
+    return render_template("c.html")
+
+@app.route("/meg")
+def meg():
+    return render_template("d.html")
+
+@app.route("/stewie")
+def stewie():
+    return render_template("e.html")
+
 @app.route("/websearch", methods = ['GET', 'POST'])
 def web_search():
     if request.method == 'POST':
@@ -14,6 +34,7 @@ def web_search():
             return render_template('index.html')
         
         return render_template('results.html', data = query)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
